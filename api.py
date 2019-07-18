d9c3e0395d2bcd4d184ff0518ef70b5ac5d290d8
Nick Amin <amin.nj@gmail.com>
2019-07-18 14:29:14 -0700

    remove test code and allow for different datasets in the phedex site presence query

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import cookielib
 import datetime
 import time
 from pprint import pprint
+from collections import defaultdict
 
 from config import API_URLS, AUTH_PATHS
 from utils import transform_input, transform_output
@@ -297,18 +298,19 @@ class PhedexApi(BaseApi):
         self.update_url_stack(r)
         self.maybe_raise_exception(r,"Phedex")
         blocks = js["phedex"]["block"]
-        d_sites = {}
-        ngbtot = 0.01
+        d_sites = defaultdict(lambda:defaultdict(lambda:0.01))
+        d_ngbtot = defaultdict(lambda:0.01)
         for block in blocks:
-            ngbtot += block["bytes"]/(1.e9)
+            dataset = block["name"].split("#",1)[0]
+            d_ngbtot[dataset] += block["bytes"]/(1.e9)
             for replica in block["replica"]:
                 if not replica["complete"]: continue
                 site = replica["node"]
-                ngb = replica["bytes"]/(1.e9)
-                if site not in d_sites: d_sites[site] = 0.01
-                d_sites[site] += ngb
-        sitepairs = [[s,round(d_sites[s]/ngbtot,3)] for s in d_sites]
-        js = {"dataset": dataset, "site_fractions": sorted(sitepairs,key=lambda k: k[1], reverse=True)}
+                d_sites[dataset][site] += replica["bytes"]/(1.e9)
+        js = []
+        for dataset in d_sites.keys():
+            sitepairs = [[s,round(d_sites[dataset][s]/d_ngbtot[dataset],3)] for s in d_sites[dataset]]
+            js.append({"dataset": dataset, "site_fractions": sorted(sitepairs,key=lambda k: k[1], reverse=True)})
         return self.make_response(data=js)
 
 class MCMApi(BaseApi):
@@ -451,61 +453,6 @@ class XSDBApi(BaseApi):
         js = r.json()
         self.maybe_raise_exception(r,"XSDB")
         return self.make_response(data=js)
-
-
-# enable_requests_caching("test3",expire_after=3000)
-f = Fetcher()
-db = SNTDBInterface(fname="allsamples.db")
-
-def do_query(query, query_type, short=True):
-
-    entity, selectors, pipes = transform_input(query)
-
-    if query_type == "snt": ret = SNTApi(db=db).get_samples(entity, selectors, short)
-
-    elif query_type == "basic":
-        if "*" in entity:
-            ret = DBSApi(fetcher=f).get_list_of_datasets(entity, short=short)
-        else:
-            ret = DBSApi(fetcher=f).get_dataset_event_count(entity)
-
-    elif query_type == "files": ret = DBSApi(fetcher=f).get_dataset_files(entity, selectors=selectors, max_files=(10 if short else None), to_dict=True)
-
-    elif query_type == "runs": ret = DBSApi(fetcher=f).get_dataset_runs(entity)
-
-    elif query_type == "config": ret = DBSApi(fetcher=f).get_dataset_config(entity)
-
-    elif query_type == "mcm": ret = MCMApi(fetcher=f).get_driver_chain_from_dataset(entity, first_only=True)
-
-    elif query_type == "parents": ret = DBSApi(fetcher=f).get_dataset_parents(entity)
-
-    elif query_type == "chain": ret = MCMApi(fetcher=f).get_driver_chain_from_dataset(entity, first_only=False)
-
-    elif query_type == "update_snt": ret = SNTApi(db=db).update_sample(entity,selectors)
-
-    elif query_type == "delete_snt": ret = SNTApi(db=db).delete_sample(entity,selectors)
-
-    elif query_type == "dbs": ret = DBSApi(fetcher=f).get_arbitrary_url(entity)
-
-    elif query_type == "xsdb": ret = XSDBApi(fetcher=f).get_samples(entity)
-
-    elif query_type == "sites":
-        if "/store/" in entity:
-            ret = PhedexApi(fetcher=f).get_file_replicas(entity)
-        else:
-            if short:
-                if "*" in entity:
-                    datasets = DBSApi(fetcher=f).get_list_of_datasets(entity, short=True)["payload"]
-                    ret = PhedexApi(fetcher=f).get_dataset_replica_fractions(datasets)
-                else:
-                    ret = PhedexApi(fetcher=f).get_dataset_replica_fractions(entity)
-            else:
-                ret = PhedexApi(fetcher=f).get_file_replicas(entity,typ="dataset")
-
-    if pipes:
-        ret["payload"] = transform_output(ret["payload"],pipes)
-
-    return ret
 
 if __name__ == "__main__":
 
