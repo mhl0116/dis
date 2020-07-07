85f57d1d57f08662a51fc73db2a34213ee78a733
Nick Amin <amin.nj@gmail.com>
2020-07-07 13:29:07 -0700

    updates

--- start.sh
+++ start.sh
@@ -8,11 +8,7 @@ cd -;
 export LANG=en_US.UTF-8
 
 
-# Now make our virtualenv for python packages separate from CMSSW
-# CMSSW still creeps in (wtf) so need two exports
-[ -d virtualenv ] || pip install virtualenv --target=`pwd`/virtualenv virtualenv
-export PYTHONPATH=`pwd`/virtualenv:$PYTHONPATH
-[ -d myenv ] || python virtualenv/virtualenv.py myenv
+[ -d myenv ] || virtualenv myenv
 source myenv/bin/activate
 export PYTHONPATH=`pwd`/myenv/lib/python2.7/site-packages:$PYTHONPATH
 pip install requests requests-cache flask pygments
